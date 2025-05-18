3f202e2bc805bc77906a2028c8de42fe64b642fe
Jose Cunha <cunhajo_code@att.net>
2025-05-18 09:47:11 -0400

    Enhanced the tip calculator written ap to add styles. Will continue with the course and then revisit to add control to select number of diners and automatically add required tip percentage selector controls after covering effects.

--- Challenges/TipCalculator/Written/src/App.jsx
+++ Challenges/TipCalculator/Written/src/App.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <TipCalculator />
     </div>
   );
@@ -21,7 +21,7 @@ function TipCalculator() {
   const totalBillAmount = billAmount + calcTip;
 
   return (
-    <div>
+    <div className="calculator">
       <BillInput
         billInput={billInput}
         setBillInput={setBillInput}
@@ -29,10 +29,18 @@ function TipCalculator() {
       >
         Enter the bill amount...
       </BillInput>
-      <SelectPercentage percentage={pct1} onPctChange={setPct1}>
+      <SelectPercentage
+        percentage={pct1}
+        onPctChange={setPct1}
+        themeClass="odd "
+      >
         How did guest 1 find the service?
       </SelectPercentage>
-      <SelectPercentage percentage={pct2} onPctChange={setPct2}>
+      <SelectPercentage
+        percentage={pct2}
+        onPctChange={setPct2}
+        themeClass="even "
+      >
         How did guest 2 find the service?
       </SelectPercentage>
       <Output
@@ -41,7 +49,13 @@ function TipCalculator() {
         calcTip={calcTip}
         totalBillAmount={totalBillAmount}
       />
-      <Reset />
+      <Reset
+        setBillInput={setBillInput}
+        setBillAmount={setBillAmount}
+        setPct1={setPct1}
+        setPct2={setPct2}
+        promptText="Are you sure you want to reset the tip calculator...?"
+      />
     </div>
   );
 }
@@ -69,7 +83,7 @@ function BillInput({ billInput, setBillInput, setBillAmount, children }) {
   }
 
   return (
-    <div className="container">
+    <div className="section input-section">
       <div className="input-row">
         <label htmlFor="bill">How much was the bill?</label>
         <input
@@ -85,40 +99,69 @@ function BillInput({ billInput, setBillInput, setBillAmount, children }) {
   );
 }
 
-function SelectPercentage({ children, percentage, onPctChange }) {
+function SelectPercentage({ children, percentage, onPctChange, themeClass }) {
   return (
-    <div>
-      <label>{children}</label>
-      <select
-        value={percentage}
-        onChange={(e) => onPctChange(Number(e.target.value))}
-      >
-        <option value="" disabled hidden>
-          -- Select tip percentage --
-        </option>
-        <option value={0}>Dissatisfied (0%)</option>
-        <option value={5}>Okay(5%)</option>
-        <option value={10}>Good(10%)</option>
-        <option value={15}>Above Average(15%)</option>
-        <option value={20}>Excellent (20%)</option>
-      </select>
+    <div className={`section select-section ${themeClass}`}>
+      <div className="select-row">
+        <label>{children}</label>
+        <select
+          value={percentage}
+          onChange={(e) => onPctChange(Number(e.target.value))}
+        >
+          <option value="" disabled hidden>
+            -- Select tip percentage --
+          </option>
+          <option value={0}>Dissatisfied (0%)</option>
+          <option value={5}>Okay(5%)</option>
+          <option value={10}>Good(10%)</option>
+          <option value={15}>Above Average(15%)</option>
+          <option value={20}>Excellent (20%)</option>
+        </select>
+      </div>
     </div>
   );
 }
 
 function Output({ billAmount, calcTipPct, calcTip, totalBillAmount }) {
   return (
-    <h4>
-      Your suggested total bill is ${billAmount ? billAmount.toFixed(2) : "-"} +{" "}
-      {calcTip === null || calcTip === 0 ? "-" : calcTip.toFixed(2)} ({" "}
-      {calcTipPct === null || calcTipPct === 0 ? "-" : calcTipPct} % Tip) = $
-      {!totalBillAmount ? "-" : totalBillAmount.toFixed(2)}
-    </h4>
+    <div className="section output-section">
+      <h4 className="output-text">
+        Your suggested total bill is{" "}
+        <span className="num">${billAmount ? billAmount.toFixed(2) : "-"}</span>{" "}
+        +{" "}
+        <span className="num">
+          {calcTip === null || calcTip === 0 ? "-" : calcTip.toFixed(2)}
+        </span>
+        {" ("}
+        <span className="num">
+          {calcTipPct === null || calcTipPct === 0 ? "-" : calcTipPct}
+        </span>{" "}
+        % Tip) = $
+        <span className="num total">
+          {!totalBillAmount ? "-" : totalBillAmount.toFixed(2)}
+        </span>
+      </h4>
+    </div>
   );
 }
 
-function Reset() {
-  return <button>Reset</button>;
+function Reset({ setBillInput, setBillAmount, setPct1, setPct2, promptText }) {
+  function resetCalculator() {
+    if (window.confirm(promptText)) {
+      setBillInput("");
+      setBillAmount(null);
+      setPct1("");
+      setPct2("");
+    }
+  }
+
+  return (
+    <div className="reset-section">
+      <button id="reset" onClick={resetCalculator}>
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default App;
